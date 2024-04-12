57284057102b668b498ea20bb7185ad19a0712c9
bagoteeth <1910610016@qq.com>
2024-04-12 18:16:00 +0800

    全局字数，时长 退出阅读状态部分逻辑 书籍持久化改为用[]，书暂用base64转成string
    详情页：
    时长，状态
    字数，目录待处理，需看下怎么从storage获取出epub
    书签，新建，删除按钮部分
    点继续阅读，目录，书签，跳转入口

--- background.js
+++ background.js
@@ -5,12 +5,26 @@ chrome.runtime.onInstalled.addListener(function () {
     console.log("插件已安装");
     chrome.storage.local.get('textSize', function (data) {
         const previousSize = data.textSize || 200;
-        chrome.storage.local.set({ textSize: previousSize })
+        chrome.storage.local.set({ textSize: previousSize });
+    });
+    chrome.storage.local.get('readingTimeTotal', function (data) {
+        const previousSize = data.readingTimeTotal || 0;
+        chrome.storage.local.set({ readingTimeTotal: previousSize });
+    });
+    chrome.storage.local.get('wordCountTotal', function (data) {
+        const previousSize = data.wordCountTotal || 0;
+        chrome.storage.local.set({ wordCountTotal: previousSize });
+    });
+    chrome.storage.local.get('globalReadStatus', function (data) {
+        const previousSize = data.globalReadStatus || false;
+        chrome.storage.local.set({ globalReadStatus: previousSize });
     });
 });
 
 chrome.storage.onChanged.addListener(function (changes, namespace) {
     for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+        oldValue = JSON.stringify(oldValue);
+        newValue = JSON.stringify(newValue);
         console.log(
             `
             Storage key "${key}" in namespace "${namespace}" changed. 
@@ -20,32 +34,5 @@ chrome.storage.onChanged.addListener(function (changes, namespace) {
             ${newValue}
             `
         );
-    }
-});
-
-// 没用
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.action === 'addFiles') {
-        const selectedFiles = request.files;
-        chrome.storage.local.get({ files: [] }, function (result) {
-            const tmpFiles = result.files;
-            for (const file of selectedFiles) {
-                tmpFiles.push(file.name);
-            }
-            chrome.storage.local.set({ files: tmpFiles });
-        });
-    }
-    else if (request.action === 'deleteFiles') {
-        const filesToDelete = request.files;
-        chrome.storage.local.get({ files: [] }, function (result) {
-            const tmpFiles = result.files.filter(fileName => !filesToDelete.includes(fileName));
-            chrome.storage.local.set({ files: tmpFiles });
-        });
-    }
-    else if (request.action === 'getFiles') {
-        chrome.storage.local.get({ files: [] }, function (result) {
-            sendResponse({ files: result.files });
-        });
-        return true; // Ensure sendResponse is called asynchronously
     }
 });
--- popup.js
+++ popup.js
@@ -1,4 +1,10 @@
 'use strict';
+const statusPreRead = "未阅读";
+const statusOnRead = "阅读中";
+const statusPostRead= "已读完";
+// overviewContainer
+const wordCountTotal = document.getElementById('wordCountTotal');
+const readingTimeTotal = document.getElementById('readingTimeTotal');
 // fileListContainer
 const addButton = document.getElementById('addButton');
 const deleteButton = document.getElementById('deleteButton');
@@ -11,27 +17,35 @@ const detailPageFileName = document.getElementById('bookTitle');
 const detailPageWordCount = document.getElementById('wordCount');
 const detailPageTime = document.getElementById('readingTime');
 const detailPageStatus = document.getElementById('bookStatus');
-const tocList = document.getElementById('tocList');
-const bookmarksList = document.getElementById('bookmarksList');
+const tocList = document.getElementById('tocList').querySelector('tbody');
+const bookmarksList = document.getElementById('bookmarksList').querySelector('tbody');
+const selectAllBookmarks = document.getElementById('selectAllBookmarks');
+const addBookmarkButton = document.getElementById('addBookmarkButton');
 const deleteBookmarkButton = document.getElementById('deleteBookmarkButton');
 const continueReadingButton = document.getElementById('continueReadingButton');
 const detailPageCloseButton = document.getElementById('closeButton');
 // textSizeContainer
-const textSizeModifyButton = document.getElementById('textSizeModifyButton')
-const textSizeInput = document.getElementById('textSizeInput')
-const textSizeSaveButton = document.getElementById('textSizeSaveButton')
+const textSizeModifyButton = document.getElementById('textSizeModifyButton');
+const textSizeInput = document.getElementById('textSizeInput');
+const textSizeSaveButton = document.getElementById('textSizeSaveButton');
+
+var currentBookObj;
 
 document.addEventListener('DOMContentLoaded', function () {
+  initOverview();
   initializeFileList();
   initializeTextSize();
 
   addButton.addEventListener('click', handleAddButtonClick);
   deleteButton.addEventListener('click', handleDeleteButtonClick);
+  exitButton.addEventListener('click', handleExitButton);
   selectAll.addEventListener('change', handleSelectAllChange);
   fileList.addEventListener('click', handleDetailPageShown);
   fileList.addEventListener('mouseover', handleMouseOver);
   fileList.addEventListener('mouseout', handleMouseOut);
 
+  selectAllBookmarks.addEventListener('click', handleSelectAllBookmarksChange);
+  addBookmarkButton.addEventListener('click', handleAddBookmark);
   deleteBookmarkButton.addEventListener('click', handleDeleteBookmark);
   continueReadingButton.addEventListener('click', handleContinueReading);
   detailPageCloseButton.addEventListener('click', handleCloseDetailPageClick);
@@ -40,12 +54,56 @@ document.addEventListener('DOMContentLoaded', function () {
   textSizeSaveButton.addEventListener('click', handleTextSizeSaveButtonClick);
 });
 
-function handleDeleteBookmark(){
+function handleExitButton() {
+  chrome.storage.local.set({ globalReadStatus: false });
+  // todo 停止计时
+}
 
+function handleAddBookmark(){
+  // todo判断在阅读状态
+  const bookmark = {
+    chapter: getChapterByProgress(currentBookObj.bookmarkEX),
+    progress: currentBookObj.bookmarkEX,
+    createTime: new Date().toLocaleString()
+  }
+  // 页面更新
+  addBookmarkToList(bookmark);
+  // 全局变量更新
+  currentBookObj.bookmarksList.push(bookmark);
+  // 持久化
+  chrome.storage.local.get({ books: []}, function (result){
+    var tmp = result.books;
+    var index = tmp.findIndex(e => e.bookName == currentBookObj.bookName);
+    alert(JSON.stringify(tmp[index].bookmarksList));
+    alert(JSON.stringify(currentBookObj.bookmarksList));
+    tmp[index] = currentBookObj;
+    alert(JSON.stringify(tmp[index].bookmarksList));
+    chrome.storage.local.set({books: tmp});
+  });
+}
+
+function handleDeleteBookmark(){
+  const checkboxes = bookmarksList.querySelectorAll('input[type="checkbox"]');
+  for(let i = checkboxes.length-1;i>=0;i--){
+    if(checkboxes[i].checked){
+      bookmarksList.deleteRow(i);
+      currentBookObj.bookmarksList.splice(i, 1);
+    }
+  }
+  chrome.storage.local.get({ books: []}, function (result){
+    var tmp = result.books;
+    var index = tmp.findIndex(e => e.bookName == currentBookObj.bookName);
+    tmp[index] = currentBookObj;
+    chrome.storage.local.set({books: tmp});
+  });
+}
+
+function getChapterByProgress(){
+  return '';
 }
 
 function handleContinueReading(){
-
+  navigateToChapter(currentBookObj, currentBookObj.bookmarkEX);
 }
 
 function handleTextSizeModifyButtonClick() {
@@ -66,13 +124,26 @@ function handleCloseDetailPageClick() {
 
 function initializeFileList() {
   //获取本地记录
-  chrome.storage.local.get({ files: [] }, function (result) {
-    const tmpFiles = result.files;
-    tmpFiles.forEach(file => addFileToTable(file));
+  chrome.storage.local.get({ books: [] }, function (result) {
+    const tmpFiles = result.books;
+    for(let book of tmpFiles){
+      addFileToTable(book.bookName);
+    }
   });
   // chrome.runtime.sendMessage({ action: 'getFiles' }, function (response) {
   //   response.files.forEach(file => addFileToTable(file));
   // });
+}
+
+function initOverview() {
+  chrome.storage.local.get('readingTimeTotal', function (data) {
+    const previousSize = data.readingTimeTotal || 0;
+    readingTimeTotal.value = data.readingTimeTotal;
+  });
+chrome.storage.local.get('wordCountTotal', function (data) {
+    const previousSize = data.wordCountTotal || 0;
+    wordCountTotal.value = data.wordCountTotal;
+  });
 }
 
 function initializeTextSize() {
@@ -88,6 +159,14 @@ function handleAddButtonClick() {
   fileInput.multiple = true;
   fileInput.addEventListener('change', handleFileInputChange);
   fileInput.click();
+
+  // chrome.fileSystem.chooseEntry({type: 'openFile', accepts: [{extensions: ['epub']}], accpetcMultiple: true}, function(fileEntries){
+  //   if (fileEntries && fileEntries,length > 0){
+  //     fileEntries.forEach(fe => handleFileInput(fe));
+  //   } else {
+  //     alert('未选择文件');
+  //   }
+  // });
 }
 
 function handleDeleteButtonClick() {
@@ -95,18 +174,17 @@ function handleDeleteButtonClick() {
   const checkboxes = document.querySelectorAll('.file-row input[type="checkbox"]');
   for (let i = checkboxes.length - 1; i >= 0; i--) {
     if (checkboxes[i].checked) {
-      // 不能同名
       filesToDelete.push(checkboxes[i].parentElement.nextElementSibling.textContent);
-      // filelist开头多一行标题，checkboxes相当于从第二行开始
+      // filelist开头多一行标题
       fileList.deleteRow(i + 1);
     }
   }
   // chrome.runtime.sendMessage({ action: 'deleteFiles', files: filesToDelete });
-  chrome.storage.local.get({ files: [] }, function (result) {
-    const tmpFiles = result.files.filter(function (fileName) {
-      return !filesToDelete.includes(fileName);
+  chrome.storage.local.get({ books: [] }, function (result) {
+    const tmpFiles = result.books.filter(function (book) {
+      return !filesToDelete.includes(book.bookName);
     })
-    chrome.storage.local.set({ files: tmpFiles });
+    chrome.storage.local.set({ books: tmpFiles });
   });
 }
 
@@ -114,6 +192,62 @@ function handleSelectAllChange() {
   const checkboxes = document.querySelectorAll('.file-row input[type="checkbox"]');
   checkboxes.forEach((checkbox) => {
     checkbox.checked = selectAll.checked;
+  });
+}
+
+function handleSelectAllBookmarksChange() {
+  const checkboxes = bookmarksList.querySelectorAll('input[type="checkbox"]');
+  checkboxes.forEach((checkbox) => {
+    checkbox.checked = selectAllBookmarks.checked;
+  });
+}
+
+// 每个文件调用一次
+function handleFileInput(fe) {
+  var fileName = fe.name;
+
+  fe.file(file => {
+    var reader = new FileReader;
+    reader.onload = function(event){
+      var fileContent = event.target.result;
+      chrome.fileSystem.getWritableEntry(fe, function(writableEntry){
+        // 重名不进行操作
+        chrome.fileSystem.restoreEntry(fileName ,function(existingEntry){
+          if(existingEntry){
+            alert('存在重名文件，不进行操作：', fileName);
+            return;
+          }
+        });
+
+        writableEntry.createWriter(function(writer){
+          writer.onerror = function(err){
+            alert('写入文件错误', err);
+          };
+          writer.onwriteend = function(){
+            chrome.storage.local.get({books: []}, function(result){
+              const tmpFiles = result.books || {};
+              tmpFiles.push({
+                bookName: fileName,
+                url: writableEntry.toURL(),
+                readTime: 0,
+                readStatus: statusPreRead,
+                /* bookmarksList{
+                  chapter: xxx,
+                  progress: xxx,
+                  createTime: xxx
+                } */
+                bookmarksList: [],
+                bookmarkEX: 0,
+              });
+              chrome.storage.local.set({ books: tmpFiles });
+            });
+          };
+          writer.write(new Blob([fileContent]));
+        });
+      });
+    };
+    reader.readAsArrayBuffer(file);
+    addFileToTable(fileName);
   });
 }
 
@@ -130,13 +264,60 @@ function handleFileInputChange(event) {
   }
   // 持久化
   // chrome.runtime.sendMessage({ action: 'addFiles', files: selectedFiles})
-  chrome.storage.local.get({ files: [] }, function (result) {
-    const tmpFiles = result.files;
+  chrome.storage.local.get({ books: [] }, function (result) {
+    const tmpFiles = result.books || [];
     for (const file of selectedFiles) {
-      tmpFiles.push(file.name);
+      blob2Base64(file2Blob(file)).then(bs64 => {
+        tmpFiles.push({
+          // fixme
+          bookName: file.name,
+          readTime: 0,
+          readStatus: statusPreRead,
+          /* {
+            chapter: xxx,
+            progress: xxx,
+            createTime: xxx
+          } */
+          bookmarksList: [],
+          bookmarkEX: 0,
+          // 文件本体
+          base64:bs64,
+        });
+        chrome.storage.local.set({ books: tmpFiles });
+      }).catch(err => {
+        alert(err);
+      });
     }
-    chrome.storage.local.set({ files: tmpFiles });
   });
+}
+
+function file2Blob(file){
+  return new Blob([file], {type: file.type});
+}
+
+function blob2Base64(blob){
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(blob);
+    reader.onloadend = () => {
+      var base64 = reader.result; 
+      resolve(base64);
+    };
+    reader.onerror = err => {
+      reject(err);
+    }
+  });
+}
+
+function base642Blob(bs64){
+  const mimeType = 'application/epub+zip';
+  const byteCharacters = atob(bs64);
+  const byteNumbers = [];
+  for(let i = 0; i < byteCharacters.length; i++){
+    byteNumbers.push(byteCharacters.charCodeAt(i));
+  }
+  const byteArr = new Uint8Array(byteNumbers);
+  return new Blob([byteArr], {type: mimeType});
 }
 
 function checkDuplicate(fileNameText) {
@@ -155,18 +336,109 @@ function addFileToTable(fileNameText) {
   const checkbox = document.createElement('input');
   checkbox.type = 'checkbox';
   checkboxCell.appendChild(checkbox);
-  // 暂时只显示文件名
   const fileName = document.createTextNode(fileNameText);
   filenameCell.appendChild(fileName);
 }
 
 function handleDetailPageShown(event) {
-  if (event.target && event.target.tagName === 'TD' && event.target.parentElement.rowIndex > 0 && event.target.cellIndex === 1) {
-    detailPageFileName.textContent = event.target.textContent.trim();
-    //加载字数，时间，状态，目录，书签
+  if (!(event.target && event.target.tagName === 'TD' && event.target.parentElement.rowIndex > 0 && event.target.cellIndex === 1)) {
+    return;
+  }
+  const bookName = event.target.textContent.trim();
+  chrome.storage.local.get({ books: []}, function (result){
+    currentBookObj = result.books.find(book => {
+      return book.bookName.trim() == bookName;
+    });
+    // 加载书名，阅读时间，状态，书签
+    detailPageStatus.textContent = currentBookObj.readStatus;
+    detailPageTime.textContent = currentBookObj.readTime;
+    detailPageFileName.textContent = bookName;
+    bookmarksList.innerHTML = '';
+    currentBookObj.bookmarksList.forEach(bookmark => addBookmarkToList(bookmark));
+    //加载字数，目录
+    // fixme
+    // var bookItem;
+    // chrome.fileSystem.retainEntry(currentBookObj.url, fe => {
+    //   fe.file(file => {
+    //     var blob = file.slice(0, file.size, file.type);
+    //     var reader = new FileReader();
+    //     reader.onload = function(event){
+    //       var arrayBuffer = event.target.result;
+    //       var uint8Array = new Uint8Array(arrayBuffer);
+    //       var epubResource = new Blob([uint8Array], { type: file.type });
+    //       bookItem = epubResource;
+    //     };
+    //     reader.readAsArrayBuffer(blob);
+    //   });
+    // });
+  
+    // const bookEpub = ePub(bookItem);
+  
+    // alert('目录：', bookEpub.getToc);
+    // alert('字数：',bookEpub.locations.charLength());
+    // bookParser.open(currentBookObj.url).then(function (book){
+    //   tocList.innerHTML = '';
+    //   // fixme
+    //   book.spine.forEach((chapter, index) => {
+    //     const row = document.createElement('tr');
+    //     const chapterCell = document.createElement('td');
+    //     chapterCell.textContent = chapter.title;
+    //     row.appendChild(chapterCell);
+    //     const progressCell = document.createElement('td');
+    //     const progress = (index + 1) / book.spine.length * 100;
+    //     progressCell.textContent = `${progress.toFixed(2)}%`;
+    //     row.appendChild(progressCell);
+    //     row.style.cursor = 'pointer';
+    //     row.addEventListener('click', () => {
+    //         navigateToChapter(currentBookObj, chapter.href);
+    //     });
+  
+    //     tocList.appendChild(row);
+    //   });
+  
+    //   // fixme
+    //   book.getBlob().then(blob => {
+    //     const reader = new FileReader();
+    //     reader.onload = function(event){
+    //       // 统计中文
+    //       const pattern = /[\u4e00-\u9fa5]/g;
+    //       const matches = event.target.result.match(pattern);
+    //       detailPageWordCount.textContent = matches ? matches.length : 0;
+    //     };
+    //   });
+    // });
     
     detailPage.style.display = 'block';
-  }
+  });
+}
+
+function addBookmarkToList(bookmark){
+  const row = bookmarksList.insertRow();
+  const checkboxCell = row.insertCell(0);
+  const chapterCell = row.insertCell(1);
+  const progressCell = row.insertCell(2);
+  const createTimeCell = row.insertCell(3);
+  
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkboxCell.appendChild(checkbox);
+  chapterCell.textContent = bookmark.chapter;
+  progressCell.textContent = bookmark.progress;
+  createTimeCell.textContent = bookmark.createTime;
+
+  [chapterCell, progressCell, createTimeCell].forEach(cell => {
+    cell.style.cursor = 'pointer';
+    cell.addEventListener('click', () => {
+      navigateToChapter(currentBookObj, bookmark.progress);
+    })
+  });
+}
+
+// 让特殊书签跳转到对应index（特殊书签，目录，书签）
+function navigateToChapter(bookObj, href){
+  bookObj.bookmarkEX = href;
+  chrome.storage.local.set({ globalReadStatus: true });
+  // todo 其他
 }
 
 function handleMouseOver(event) {
